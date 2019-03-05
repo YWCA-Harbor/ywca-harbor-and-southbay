06840eee7b7df15d5a0c36c68bf236d75d6832bb
Gregory Salinas <asalinas720@gmail.com>
2019-03-05 14:53:58 -0800

    Events and Opportunities Vuex state is ready

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,10 @@ import Nav from "./partials/nav";
 export default {
   components: {
     Nav
+  },
+  mounted() {
+    this.$store.dispatch("events/getEvents");
+    this.$store.dispatch("opportunities/getOpportunities");
   }
 };
 </script>
--- plugins/ywca-api.js
+++ plugins/ywca-api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://ywca-harbor-and-southbay.firebaseio.com/flamelink/environments/production/'
+  baseURL: 'https://ywca-harbor-and-southbay.firebaseio.com/flamelink/environments/production/content'
 })
