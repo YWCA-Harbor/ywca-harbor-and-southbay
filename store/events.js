41a25f32cb9e4e66cf7923fa58829a2e37baaf59
Gregory Salinas <asalinas720@gmail.com>
2019-03-06 12:48:13 -0800

    Reduced API calls to only one page

--- store/events.js
+++ store/events.js
@@ -1,7 +1,8 @@
 import axios from '../plugins/ywca-api';
 
 export const state = () => ({
-  events: {}
+  events: {},
+  calendarObj: {}
 });
 
 export const actions = {
@@ -11,11 +12,45 @@ export const actions = {
     let res = await axios.get(`/calendarEvents/en-US.json`);
     let data = res.data;
     commit('setEvents', data);
+    commit('setCalendarObj', data);
   }
 };
 
 export const mutations = {
   setEvents(state, events) {
     state.events = events;
+  },
+  setCalendarObj(state, events) {
+    let calendarObj = {};
+    for (let event in events) {
+      let date = events[event].date.split("T")[0];
+      let year = date.split("-")[0];
+      let month = date.split("-")[1];
+      let day = date.split("-")[2];
+      let atTime = new Date(`${date}T${events[event].time}`);
+      let endTime = new Date(`${date}T${events[event].endTime}`);
+      if (!calendarObj[year]) {
+        calendarObj[year] = {};
+      }
+      if (!calendarObj[year][month - 1]) {
+        calendarObj[year][month - 1] = {};
+      }
+      if (!calendarObj[year][month - 1][day]) {
+        calendarObj[year][month - 1][day] = [{
+          displayname: events[event].eventName,
+          duration: endTime,
+          at: atTime,
+          id: event
+        }];
+      } else {
+        calendarObj[year][month - 1][day].push({
+          displayname: events[event].eventName,
+          duration: endTime,
+          at: atTime,
+          id: event
+        });
+      }
+    }
+    state.calendarObj = calendarObj;
   }
 };
